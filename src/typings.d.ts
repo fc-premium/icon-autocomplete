efff03d83ca12486d4d447741fee81fb0479012b
Pytness <pytness@gmail.com>
2020-07-11 20:13:56 +0200

    Added typings for external lib

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,3 +2,9 @@ declare module '*.css' {
 	const value: string;
 	export = value;
 }
+
+/// <reference types="jquery" />
+declare module '@fc-lib/jquery' {
+	var _: JQueryStatic;
+	export default _;
+}
