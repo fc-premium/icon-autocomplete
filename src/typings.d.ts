5e64dc6789a51441a3a8b39cf71d746110408c63
Fernando Vega <pytness@gmail.com>
2022-05-20 11:06:47 +0200

    Add moustrap types

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -8,3 +8,9 @@ declare module '@fc-lib/jquery' {
 	var _: JQueryStatic;
 	export default _;
 }
+
+/// <reference types="mousetrap" />
+declare module '@fc-lib/mousetrap' {
+	var _: typeof Mousetrap;
+	export default _;
+}
